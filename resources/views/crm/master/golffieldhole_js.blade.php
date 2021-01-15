c00867cd0675a119145e619d0de3a0ab3f6bf1bd
ahmad farhan h <ahmadfarhanhafidzudin555@gmail.com>
2021-01-15 15:23:57 +0700

    fix search datatable

--- resources/views/crm/master/golffieldhole_js.blade.php
+++ resources/views/crm/master/golffieldhole_js.blade.php
@@ -13,44 +13,13 @@
       }
   
   
-      $("#gfh").DataTable({
+      $("#gfhTable").DataTable({
         "order": [[ 0, "desc" ]],
         dom: '<"top"fB>rt<"bottom"lip><"clear">',
         lengthMenu: [
             [ 10, 25, 50,100, -1 ],
             [ '10 rows', '25 rows', '50 rows','100 rows', 'Show all' ]
-        ],
-        buttons: [
-            {
-                extend: 'copyHtml5',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5, 6]
-                }
-            },
-            {
-                extend: 'csv',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5, 6]
-                }
-            },
-            {
-                extend: 'excel',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5, 6]
-                }
-            },
-            {
-                extend: 'pdf',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5, 6]
-                }
-            },
-            {
-                extend: 'print',
-                exportOptions: {
-                    columns: [ 0, 1, 2, 3, 4, 5, 6]
-                }
-            },
+        ]
         
       });
   
