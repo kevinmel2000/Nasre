6ba5959919b0f6178129b57e39b72a9376cea804
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-01-24 14:52:07 +0700

    file temp model

---
+++ app/Models/SlipTableFileTemp.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\MOdels;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SlipTable extends Model
+{
+    //
+    protected $guarded = [];
+
+    protected $table = 'slip_table_file_temp';
+}
