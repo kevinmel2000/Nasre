81f1f35d63a4817e41a65333cadee3b43ae7ea6a
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-01-25 16:09:29 +0700

    Model Location Temp

---
+++ app/Models/TransLocationTemp.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\MOdels;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TransLocationTemp extends Model
+{
+    //
+    protected $guarded = [];
+
+    protected $table = 'trans_location_temp';
+}
