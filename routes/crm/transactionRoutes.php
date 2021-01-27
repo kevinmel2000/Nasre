49f1f75a2aec1252a7ddda723b9da8e4413f8209
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-01-27 14:21:34 +0700

    perbaikan route

--- routes/crm/transactionRoutes.php
+++ routes/crm/transactionRoutes.php
@@ -29,7 +29,7 @@ Route::group(['prefix'=>'/transaction-data','middleware'=>['auth']], function(){
     Route::post('/fe-slipindex', [FeSlipController::class, 'index']);
 
     Route::delete('/fe-slip/destroy/{fe}', [FeSlipController::class, 'destroy']);
-    Route::post('/fe-slip/getCostumers/','FeSlipController@getCostumers')->company_name('customer.getCostumers');
+    Route::post('/fe-slip/getCostumers/','FeSlipController@getCostumers')->name('customer.getCostumers');
 
 
     // SECTION Financial Lines Slip Group Routes
@@ -38,7 +38,7 @@ Route::group(['prefix'=>'/transaction-data','middleware'=>['auth']], function(){
     Route::post('/fl-slipindex', [FinancialLineSlipController::class, 'index']);
 
     Route::delete('/fl-slip/destroy/{fe}', [FinancialLineSlipController::class, 'destroy']);
-    Route::post('/fl-slip/getCostumers/','FinancialLineSlipController@getCostumers')->company_name('customer.getCostumers');
+    Route::post('/fl-slip/getCostumers/','FinancialLineSlipController@getCostumers')->name('customer.getCostumers');
     
     // SECTION Moveable Property Slip Group Routes
     Route::get('/mp-slip', [MovePropSlipController::class, 'indexmpslip']);
@@ -46,7 +46,7 @@ Route::group(['prefix'=>'/transaction-data','middleware'=>['auth']], function(){
     Route::post('/mp-slipindex', [MovePropSlipController::class, 'index']);
     
     Route::delete('/mp-slip/destroy/{fe}', [MovePropSlipController::class, 'destroy']);
-    Route::post('/mp-slip/getCostumers/','MovePropSlipController@getCostumers')->company_name('customer.getCostumers');
+    Route::post('/mp-slip/getCostumers/','MovePropSlipController@getCostumers')->name('customer.getCostumers');
    
 
     // SECTION HE & Motor Slip Group Routes
@@ -55,7 +55,7 @@ Route::group(['prefix'=>'/transaction-data','middleware'=>['auth']], function(){
     Route::post('/hem-slipindex', [HeMotorSlipController::class, 'index']);
 
     Route::delete('/hem-slip/destroy/{fe}', [HeMotorSlipController::class, 'destroy']);
-    Route::post('/hem-slip/getCostumers/','HeMotorSlipController@getCostumers')->company_name('customer.getCostumers');
+    Route::post('/hem-slip/getCostumers/','HeMotorSlipController@getCostumers')->name('customer.getCostumers');
    
 
     // SECTION Hole in Ones Slip Group Routes
