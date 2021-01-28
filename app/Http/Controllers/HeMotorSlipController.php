4884214e734f73df040b5e5c8d5fce42dbd3ef07
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-01-28 13:33:12 +0700

    proses

--- app/Http/Controllers/HeMotorSlipController.php
+++ app/Http/Controllers/HeMotorSlipController.php
@@ -167,7 +167,7 @@ class HeMotorSlipController extends Controller
         }
 
 
-        $locationlist= TransLocationTemp::where('lookup_location_id','=',$code_ms)->orderby('id','desc')->get();
+        $locationlist= TransLocationTemp::where('insured_id','=',$code_ms)->orderby('id','desc')->get();
 
 
         return view('crm.transaction.hem_slip', compact(['user','cnd','locationlist','felookup','currency','cob','koc','ocp','ceding','cedingbroker','route_active','currdate','slip','insured','hem_ids','code_ms','code_sl','costumer']));
