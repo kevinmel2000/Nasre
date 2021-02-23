5b165700a20260d2f9ac2fb35a1247d8c97af04c
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-02-23 12:16:05 +0700

    FL Tab Gabung

--- resources/views/crm/transaction/fl_slip.blade.php
+++ resources/views/crm/transaction/fl_slip.blade.php
@@ -238,11 +238,9 @@
                                     <a class="nav-link active" id="general-details" data-toggle="pill" href="#general-details-id" role="tab" aria-controls="general-details-id" aria-selected="true">{{__('General Data')}}</a>
                                     </li>
                                     <li class="nav-item">
-                                    <a class="nav-link" id="insured-details" data-toggle="pill" href="#insured-details-id" role="tab" aria-controls="address-details-id" aria-selected="false">{{__('Insured Data')}}</a>
+                                    <a class="nav-link" id="insured-details" data-toggle="pill" href="#insured-details-id" role="tab" aria-controls="address-details-id" aria-selected="false">{{__('Insured Data & Insurance Measurement')}}</a>
                                     </li>
-                                    <li class="nav-item">
-                                    <a class="nav-link" id="insurance-details" data-toggle="pill" href="#insurance-details-id" role="tab" aria-controls="social-media-details-id" aria-selected="false">{{__('Insurance Measurement')}}</a>
-                                    </li>
+                                    
                                     <li class="nav-item">
                                         <a class="nav-link" id="installment-details" data-toggle="pill" href="#installment-details-id" role="tab" aria-controls="installment-details-id" aria-selected="false">{{__('Installment & Retrocession')}}</a>
                                     </li>
@@ -777,8 +775,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="tab-pane fade" id="insurance-details-id" role="tabpanel" aria-labelledby="insurance-details">
+
                                         <div class="row">
                                             <div class="col-md-8">
                                                 <div class="row">
@@ -969,7 +966,9 @@
                                                 </div>
                                             </div>
                                         </div>
+
                                     </div>
+                                    
                                     <div class="tab-pane fade" id="installment-details-id" role="tabpanel" aria-labelledby="installment-details">
                                         <div class="row">
                                             <div class="col-md-12">
