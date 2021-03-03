34f8f7412719879e298e69486c4d6e62150ea357
ahmad farhan h <ahmadfarhanhafidzudin555@gmail.com>
2021-03-03 14:56:17 +0700

    update fix

--- resources/views/crm/transaction/fl_slip_js.blade.php
+++ resources/views/crm/transaction/fl_slip_js.blade.php
@@ -686,6 +686,20 @@
         $('#sliprpto').val($(this).val());
     });
 
+    $('#slipipfromupdate').change(function(){
+        $('#sliprpfromupdate').val($(this).val());
+    });
+
+    $('#slipiptoupdate').change(function(){
+        $('#sliprptoupdate').val($(this).val());
+    });
+
+    $('#slipipfromendorsement').change(function(){
+        $('#sliprpfromendorsement').val($(this).val());
+    });
+
+    $('#slipiptoendorsement').change(function(){
+        $('#sliprptoendorsement').val($(this).val());
 </script>
 
 
@@ -723,7 +737,9 @@ $(document).ready(function() {
         $("#sliplayerproportional").attr('hidden','true');
         $("#labelnonprop").attr('hidden','true');
         $("#tabretro").attr('hidden','true');
-
+        $("#tabretrodetail").attr('hidden','true');
+        $("#tabretroupdate").attr('hidden','true');
+        $("#tabretroendorsement").attr('hidden','true');
         
         $("#btn-success2").click(function(){ 
         var html = $(".clone2").html();
@@ -753,6 +769,51 @@ $(document).ready(function() {
         
     });
 
+    $('#switch-proportionaldetail').change(function(){
+        var attr = $("#btnaddlayerdetail").attr('hidden');
+        if(typeof attr !== typeof undefined && attr !== false){
+            $("#btnaddlayerdetail").removeAttr('hidden');
+            $("#sliplayerproportionaldetail").removeAttr('hidden');
+            $("#labelnonpropdetail").removeAttr('hidden');
+        }
+        else{
+            $("#btnaddlayerdetail").attr('hidden','true');
+            $("#sliplayerproportionaldetail").attr('hidden','true');
+            $("#labelnonpropdetail").attr('hidden','true');
+        }
+        
+    });
+
+    $('#switch-proportionalupdate').change(function(){
+        var attr = $("#btnaddlayerupdate").attr('hidden');
+        if(typeof attr !== typeof undefined && attr !== false){
+            $("#btnaddlayerupdate").removeAttr('hidden');
+            $("#sliplayerproportionalupdate").removeAttr('hidden');
+            $("#labelnonpropupdate").removeAttr('hidden');
+        }
+        else{
+            $("#btnaddlayerupdate").attr('hidden','true');
+            $("#sliplayerproportionalupdate").attr('hidden','true');
+            $("#labelnonpropupdate").attr('hidden','true');
+        }
+        
+    });
+
+    $('#switch-proportionalendorsement').change(function(){
+        var attr = $("#btnaddlayerendorsement").attr('hidden');
+        if(typeof attr !== typeof undefined && attr !== false){
+            $("#btnaddlayerendorsement").removeAttr('hidden');
+            $("#sliplayerproportionalendorsement").removeAttr('hidden');
+            $("#labelnonpropendorsement").removeAttr('hidden');
+        }
+        else{
+            $("#btnaddlayerendorsement").attr('hidden','true');
+            $("#sliplayerproportionalendorsement").attr('hidden','true');
+            $("#labelnonpropendorsement").attr('hidden','true');
+        }
+        
+    });
+
     $('#sliprb').change(function(){
         var attr = $("#tabretro").attr('hidden');
         if(typeof attr !== typeof undefined && attr !== false){
@@ -764,6 +825,43 @@ $(document).ready(function() {
             $("#tabretro").attr('hidden','true');
         }
     });
+
+    $('#sliprbdetail').change(function(){
+        var attr = $("#tabretrodetail").attr('hidden');
+        if(typeof attr !== typeof undefined && attr !== false){
+            // $("#retrocessionPanel").removeAttr('hidden');
+            $("#tabretrodetail").removeAttr('hidden');
+        }
+        else{
+            // $("#retrocessionPanel").attr('hidden','true');
+            $("#tabretrodetail").attr('hidden','true');
+        }
+    });
+
+    $('#sliprbupdate').change(function(){
+        var attr = $("#tabretroupdate").attr('hidden');
+        if(typeof attr !== typeof undefined && attr !== false){
+            // $("#retrocessionPanel").removeAttr('hidden');
+            $("#tabretroupdate").removeAttr('hidden');
+        }
+        else{
+            // $("#retrocessionPanel").attr('hidden','true');
+            $("#tabretroupdate").attr('hidden','true');
+        }
+    });
+
+    $('#sliprbendorsement').change(function(){
+        var attr = $("#tabretroendorsement").attr('hidden');
+        if(typeof attr !== typeof undefined && attr !== false){
+            // $("#retrocessionPanel").removeAttr('hidden');
+            $("#tabretroendorsement").removeAttr('hidden');
+        }
+        else{
+            // $("#retrocessionPanel").attr('hidden','true');
+            $("#tabretroendorsement").attr('hidden','true');
+        }
+    });
+
 </script>
 
 
