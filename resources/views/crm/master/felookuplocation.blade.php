ffd5966b8f6c3f173622e00f24388fdaad242768
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-03-11 23:49:45 +0700

    Fe Location Searching Map FIx

--- resources/views/crm/master/felookuplocation.blade.php
+++ resources/views/crm/master/felookuplocation.blade.php
@@ -3,10 +3,53 @@
 <style type="text/css">    
       #map {
         margin: 10px;
-        width: 600px;
-        height: 300px;  
+        width: 650px;
+        height: 450px;  
         padding: 10px;
       }
+
+.pac-card {
+  margin: 10px 10px 0 0;
+  border-radius: 2px 0 0 2px;
+  box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  outline: none;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+  background-color: #fff;
+  font-family: Roboto;
+}
+
+#pac-container {
+  padding-bottom: 12px;
+  margin-right: 12px;
+}
+
+.pac-controls {
+  display: inline-block;
+  padding: 5px 11px;
+}
+
+.pac-controls label {
+  font-family: Roboto;
+  font-size: 13px;
+  font-weight: 300;
+}
+
+#pac-input {
+  background-color: #fff;
+  font-family: Roboto;
+  font-size: 15px;
+  font-weight: 300;
+  margin-left: 12px;
+  padding: 0 11px 0 13px;
+  text-overflow: ellipsis;
+  width: 400px;
+}
+
+#pac-input:focus {
+  border-color: #4d90fe;
+}
+
 </style>
 
 @section('content')
@@ -158,6 +201,12 @@
                 <div class="form-group">
                      
                     <div class="col-md-12">
+                       <input
+                          id="pac-input"
+                          class="controls"
+                          type="text"
+                          placeholder="Search Box"
+                        />
                        <div id="map"></div>
                     </div>
 
--- resources/views/crm/master/felookuplocation_js.blade.php
+++ resources/views/crm/master/felookuplocation_js.blade.php
@@ -123,7 +123,7 @@
 
 
 
-<script src="//maps.google.com/maps/api/js?sensor=false&amp;key=AIzaSyD4yhKiCvJpWtee_7Bobk_9qjDUAZTTOKE" type="text/javascript"></script>
+<script src="//maps.google.com/maps/api/js?sensor=false&key=AIzaSyD4yhKiCvJpWtee_7Bobk_9qjDUAZTTOKE&&callback=initAutocomplete&libraries=places&v=weekly" type="text/javascript"></script>
 
 <link href="{{asset('css/select2.css')}}" rel="stylesheet"/>
 <script src="{{asset('js/select2.js')}}"></script>
@@ -276,11 +276,69 @@ center: new google.maps.LatLng(-6.175392,106.827153),
 //posisi awal marker   
 var latLng = new google.maps.LatLng(-6.175392,106.827153);
 
+const input = document.getElementById("pac-input");
+const searchBox = new google.maps.places.SearchBox(input);
+
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+// Bias the SearchBox results towards current map's viewport.
+map.addListener("bounds_changed", () => {
+  searchBox.setBounds(map.getBounds());
+});
+
+let markers = [];
+
+
+searchBox.addListener("places_changed", () => {
+  const places = searchBox.getPlaces();
+
+  if (places.length == 0) {
+    return;
+  }
+  // Clear out the old markers.
+  markers.forEach((marker) => {
+    marker.setMap(null);
+  });
+  markers = [];
+  // For each place, get the icon, name and location.
+  const bounds = new google.maps.LatLngBounds();
+  places.forEach((place) => {
+    if (!place.geometry || !place.geometry.location) {
+      console.log("Returned place contains no geometry");
+      return;
+    }
+    const icon = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25),
+    };
+    // Create a marker for each place.
+    markers.push(
+      new google.maps.Marker({
+        map,
+        icon,
+        title: place.name,
+        position: place.geometry.location,
+      })
+    );
+
+    if (place.geometry.viewport) {
+      // Only geocodes have viewport.
+      bounds.union(place.geometry.viewport);
+    } else {
+      bounds.extend(place.geometry.location);
+    }
+  });
+  map.fitBounds(bounds);
+});
+
+
 /* buat marker yang bisa di drag lalu 
   panggil fungsi updateMarkerPosition(latLng)
  dan letakan posisi terakhir di id=latitude dan id=longitude
  */
-var marker = new google.maps.Marker({
+var marker2 = new google.maps.Marker({
     position : latLng,
     title : 'lokasi',
     map : map,
@@ -297,10 +355,10 @@ geocoder.geocode({
       document.getElementById('address').value=results[4].formatted_address;
 });
 
-google.maps.event.addListener(marker, 'drag', function() {
+google.maps.event.addListener(marker2, 'drag', function() {
  // ketika marker di drag, otomatis nilai latitude dan longitude
  //menyesuaikan dengan posisi marker 
-    updateMarkerPosition(marker.getPosition());
+    updateMarkerPosition(marker2.getPosition());
 });
 
 
