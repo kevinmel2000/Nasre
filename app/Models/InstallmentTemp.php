0f068eebb606b3b86c00eb99be0145eea3cbfb29
ahmad farhan h <ahmadfarhanhafidzudin555@gmail.com>
2021-04-22 14:37:51 +0700

    update

--- app/Models/InstallmentTemp.php
+++ app/Models/InstallmentTemp.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class InstallmentTemp extends Model
 {
-	use SoftDeletes;
-    protected $dates = ['deleted_at'];
+	// use SoftDeletes;
+ //    protected $dates = ['deleted_at'];
     protected $table = "installment_panel_detail";
 
     protected $guarded = [];
