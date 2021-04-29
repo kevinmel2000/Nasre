9afb2700076fdc10a7d746cf78e3a13490412a51
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-04-29 18:32:41 +0700

    Master Prefix Data Model

---
+++ app/Models/MasterPrefix.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\MOdels;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MasterPrefix extends Model
+{
+    //
+    protected $guarded = [];
+
+    protected $table = 'master_prefix';
+
+}
+
