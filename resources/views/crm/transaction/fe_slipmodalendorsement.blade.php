2ed29229bfbedbff0f44119864696ed336db4388
ahmad farhan h <ahmadfarhanhafidzudin555@gmail.com>
2021-04-30 15:34:13 +0700

    update

--- resources/views/crm/transaction/fe_slipmodalendorsement.blade.php
+++ resources/views/crm/transaction/fe_slipmodalendorsement.blade.php
@@ -985,12 +985,24 @@
 
                         <div class="card card-primary">
                             <div class="card-body">
-                                <div class="row">
-                                
-                                
-                                </div>
+                                <ul class="nav nav-tabs nav-tabs-bottom" id="custom-tabs-three-tabbottom" role="tablist">
+                                    
+                                    <li class="pt-1 px-3"><h3 class="card-title">{{__('Slip Form')}}</h3></li>
+                                    
+                                    <li class="nav-item active" href="#general-details-id">
+                                        <a class="nav-link active" id="general-details" data-toggle="pill" href="#general-details-id" role="tab" aria-controls="general-details-id" aria-selected="true">{{__('General Data')}}</a>
+                                    </li>
+
+                                    <li class="nav-item" href="#insured-details-id">
+                                        <a class="nav-link" id="insured-details" data-toggle="pill" href="#insured-details-id" role="tab" aria-controls="address-details-id" aria-selected="false">{{__('Insured Data & Insurance Measurement')}}</a>
+                                    </li>
+                                    
+                                    <li class="nav-item" href="#installment-details-id">
+                                        <a class="nav-link" id="installment-details" data-toggle="pill" href="#installment-details-id" role="tab" aria-controls="installment-details-id" aria-selected="false">{{__('Installment & Retrocession')}}</a>
+                                    </li>
+                                </ul>
                             </div>
-                        </div> 
+                        </div>
                     
 
                         
