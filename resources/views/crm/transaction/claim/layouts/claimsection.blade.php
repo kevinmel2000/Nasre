97c5852cc8e63eabb7947c124974f35efa2853d9
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-05-05 02:26:33 +0700

    Claim Form Progress

--- resources/views/crm/transaction/claim/layouts/claimsection.blade.php
+++ resources/views/crm/transaction/claim/layouts/claimsection.blade.php
@@ -72,6 +72,133 @@
 
 	</div>
 	<div class="col-md-6">
-	
+
+	</div>
+</div>
+
+
+<div class="row">
+	<div class="col-md-2">
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>Potential Recovery</td>
+					<td>	
+						<select name="potentialrecoverydecision" id="potentialrecoverydecision" class="form-control">
+							<option value="Y">Yes</option>
+							<option value="N">No</option>
+						</select>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-6">
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>
+					<textarea name="potentialrecovery" id="potentialrecovery" class="form-control" autocomplete="off"></textarea>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-4">
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>
+					Estimasi Amount Subrogasi
+					</td>
+					<td>
+					<input type="text" name="subrogasi" id="subrogasi" class="form-control datepicker" autocomplete="off">
+							
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+
+
+<div class="row">
+	<div class="col-md-2">
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>Kronologi</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-9">
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>
+					<textarea name="kronologi" id="kronologi" class="form-control" autocomplete="off"></textarea>
+					</td>
+				</tr>
+			</tbody>
+		</table>	
+	</div>
+</div>
+
+
+
+<div class="row">
+	<div class="col-md-2">
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>Staff Recommendation</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-9">
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>
+					<textarea name="staffrecomend" id="staffrecomend" class="form-control" autocomplete="off"></textarea>
+					</td>
+				</tr>
+			</tbody>
+		</table>	
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-2">
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>Assistan Manager Recommendation</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-9">
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>
+					<textarea name="assistantmanagerrecomend" id="assistantmanagerrecomend" class="form-control" autocomplete="off"></textarea>
+					</td>
+				</tr>
+			</tbody>
+		</table>	
+	</div>
+</div>
+
+
+<div class="row">
+	<div class="col-md-2">
+		<button> POST </button>
+	</div>
+	<div class="col-md-6">
+
 	</div>
 </div>
