353a12f82ab9ffdca404e925c50b72067d5fe9e0
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-05-05 02:03:18 +0700

    Claim Form Progress

--- resources/views/crm/transaction/claim/layouts/othersection.blade.php
+++ resources/views/crm/transaction/claim/layouts/othersection.blade.php
@@ -1,49 +1,71 @@
 <div class="row">
-	<div class="col-md-4">
+
+	<div class="col-md-1">	
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>Total Recovery</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="col-md-3">
 		<div class="table-responsive">
 				<table class="table">
 					<tbody>
 						<tr>
-							<th>
-								Total Recovery
-							</th>
-							<th>
+							<td>
 								<input type="text" name="totalrecovery" id="totalrecovery" class="form-control" autocomplete="off">
-							</th>
+							</td>
 						</tr>
 					</tbody>
 				</table>
 		</div>	
 	</div>
 
-	<div class="col-md-4">
+
+	<div class="col-md-1">	
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>NR's Gross Ret</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-3">
 		<div class="table-responsive">
 				<table class="table">
 					<tbody>
 						<tr>
-							<th>
-								NR's Gross Ret
-							</th>
-							<th>
+							<td>
 								<input type="text" name="nrsgrossret" id="nrsgrossret" class="form-control" autocomplete="off">
-							</th>
+							</td>
 						</tr>
 					</tbody>
 				</table>
 		</div>	
 	</div>
 
-	<div class="col-md-4">
+	<div class="col-md-1">	
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>XOL</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="col-md-3">
 			<div class="table-responsive">
 				<table class="table">
 					<tbody>
 						<tr>
-							<th>
-								XOL
-							</th>
-							<th>
+							<td>
 								<input type="text" name="xol" id="xol" class="form-control" autocomplete="off">
-							</th>
+							</td>
 						</tr>
 					</tbody>
 				</table>
@@ -52,38 +74,128 @@
 </div>
 
 <div class="row">
-	<div class="col-md-4">
-			
+
+	<div class="col-md-1">	
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>Ce Reff No</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="col-md-3">
+			<div class="table-responsive">
+				<table class="table">
+					<tbody>
+						<tr>
+							<td>
+								<input type="text" name="cereffno" id="cereffno" class="form-control" autocomplete="off">
+							</td>
+							<td>
+								<button>Nr. CE</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>	
+	</div>
+
+	<div class="col-md-1">	
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>Date Of Prod</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+
+	<div class="col-md-3">
+			<div class="table-responsive">
+				<table class="table">
+					<tbody>
+						<tr>
+							<td>
+								<input type="text" name="dateofprod" id="dateofprod" class="form-control" autocomplete="off">
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>	
 	</div>
 
 	<div class="col-md-4">
-			
-	</div>
-
-	<div class="col-md-4">
-			
+			<div class="table-responsive">
+				<table class="table">
+					<tbody>
+						<tr>
+							<th>
+								<button>Insert View Attachment</button>
+							</th>
+						</tr>
+					</tbody>
+				</table>
+			</div>	
 	</div>
 </div>
 
 
 <div class="row">
-	<div class="col-md-4">
-			
+	<div class="col-md-1">	
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>CE No</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="col-md-3">
+			<div class="table-responsive">
+				<table class="table">
+					<tbody>
+						<tr>
+							<td>
+								<input type="text" name="ceno" id="ceno" class="form-control" autocomplete="off">
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>	
 	</div>
 
-	<div class="col-md-4">
-			
+	<div class="col-md-1">	
+		<table class="table">
+			<tbody>
+				<tr>
+					<td>CE User</td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 
-	<div class="col-md-4">
-			
+	<div class="col-md-3">
+			<div class="table-responsive">
+				<table class="table">
+					<tbody>
+						<tr>
+							<td>
+								<input type="text" name="ceuser" id="ceuser" class="form-control" autocomplete="off">
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>	
 	</div>
+
 </div>
 
 
 
 <div class="row">
-	<div class="col-md-2">
+	<div class="col-md-1">
 		
 		<table class="table">
 			<tbody>
@@ -94,7 +206,7 @@
 		</table>
 		
 	</div>
-	<div class="col-md-10">
+	<div class="col-md-11">
 		<table class="table">
 			<tbody>
 				<tr>
@@ -108,7 +220,7 @@
 </div>
 
 <div class="row">
-	<div class="col-md-2">
+	<div class="col-md-1">
 		<table class="table">
 			<tbody>
 				<tr>
@@ -118,7 +230,7 @@
 		</table>
 	</div>
 
-	<div class="col-md-4">	
+	<div class="col-md-5">	
 		<table class="table">
 			<tbody>
 				<tr>
@@ -135,7 +247,7 @@
 </div>
 
 <div class="row">
-	<div class="col-md-2">
+	<div class="col-md-1">
 		<table class="table">
 			<tbody>
 				<tr>
@@ -145,7 +257,7 @@
 		</table>
 	</div>
 
-	<div class="col-md-4">
+	<div class="col-md-5">
 		<table class="table">
 			<tbody>
 				<tr>
@@ -162,7 +274,7 @@
 </div>
 
 <div class="row">
-	<div class="col-md-2">
+	<div class="col-md-1">
 		<table class="table">
 			<tbody>
 				<tr>
@@ -171,7 +283,7 @@
 			</tbody>
 		</table>
 	</div>
-	<div class="col-md-4">
+	<div class="col-md-5">
 		<table class="table">
 			<tbody>
 				<tr>
