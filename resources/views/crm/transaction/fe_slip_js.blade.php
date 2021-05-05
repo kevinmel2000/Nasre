10e3529ad41f231c2144ec000432814eaa967725
ahmad farhan h <ahmadfarhanhafidzudin555@gmail.com>
2021-05-05 13:16:55 +0700

    update

--- resources/views/crm/transaction/fe_slip_js.blade.php
+++ resources/views/crm/transaction/fe_slip_js.blade.php
@@ -304,9 +304,10 @@
                     {
                         var obj = installment_panel[i];
                         // var curr_amount = new Intl.NumberFormat('id-ID',  {style: 'currency',currency: 'IDR',}).format(obj.amount);
-                        // var conv_amount = obj.amount.toFixed(2);
-                        // var str_amount = conv_amount.toString();
-                        var curr_amount = obj.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                        var conv_amount = obj.amount.toFixed(2);
+                        var str_amount = conv_amount.toString();
+                        var curr_amount = str_amount.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                        // var curr_amount = obj.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
                         var currdate = obj.installment_date;
                         var convdate = currdate.split("-").reverse().join("/");
