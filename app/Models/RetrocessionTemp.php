ef48dc42bbdd22d6950102fefd1efb778e01eb2d
ahmad farhan h <ahmadfarhanhafidzudin555@gmail.com>
2021-05-05 14:03:58 +0700

    update

--- app/Models/RetrocessionTemp.php
+++ app/Models/RetrocessionTemp.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;\
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RetrocessionTemp extends Model
