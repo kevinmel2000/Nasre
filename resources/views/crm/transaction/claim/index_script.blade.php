f7cf7a85cfc2943817623d06858614e141b85834
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2021-05-18 12:10:48 +0700

    Claim FIx

--- resources/views/crm/transaction/claim/index_script.blade.php
+++ resources/views/crm/transaction/claim/index_script.blade.php
@@ -30,6 +30,21 @@
 </style>
 
 
+<script type="text/javascript">
+    function treatAsUTC(date) {
+        var result = new Date(date);
+        result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
+        return result;
+    }
+
+    function daysBetween(startDate, endDate) {
+        var millisecondsPerDay = 24 * 60 * 60 * 1000;
+        return (treatAsUTC(endDate) - treatAsUTC(startDate)) / millisecondsPerDay;
+    }
+
+</script>
+
+
 
 <script type="text/javascript">
   
