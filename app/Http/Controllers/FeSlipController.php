8893df40f56d242bb1667fe3e96b9c09b277d752
ahmad farhan h <ahmadfarhanhafidzudin555@gmail.com>
2021-05-21 09:11:57 +0700

    update

--- app/Http/Controllers/FeSlipController.php
+++ app/Http/Controllers/FeSlipController.php
@@ -432,7 +432,7 @@ class FeSlipController extends Controller
         
         $currency = Currency::orderby('id','asc')->get();
         $cob = COB::where('form','fe')->orderby('id','asc')->get();
-        $koc = Koc::where('parent_id',2)->orWhere('code', 'like',  02 . '%')->orWhere('code', 'like',  0202 . '%')->orWhere('code', 'like', '%' .  02 . '%')->orWhere('code','02')->orderby('code','asc')->get();
+        $koc = Koc::where('parent_id',2)->orWhere('code', 'like',  02 . '%')->orWhere('code', 'like',  0202 . '%')->orWhere('code','02')->orderby('code','asc')->get();
         $ocp = Occupation::orderby('id','asc')->get();
         $cedingbroker = CedingBroker::orderby('id','asc')->get();
         $ceding = CedingBroker::orderby('id','asc')->where('type','4')->get();
@@ -1133,7 +1133,7 @@ class FeSlipController extends Controller
         $slip = SlipTable::orderby('id','asc')->get();
         $currency = Currency::orderby('id','asc')->get();
         $cob = COB::orderby('id','asc')->get();
-        $koc = KOC::where('parent_id',2)->orWhere('code', 'like',  02 . '%')->orWhere('code', 'like',  0202 . '%')->orWhere('code', 'like', '%' .  02 . '%')->orWhere('code','02')->orderby('id','asc')->get();
+        $koc = KOC::where('parent_id',2)->orWhere('code', 'like',  02 . '%')->orWhere('code', 'like',  0202 . '%')->orWhere('code','02')->orderby('id','asc')->get();
         $ocp = Occupation::orderby('id','asc')->get();
         $cedingbroker = CedingBroker::orderby('id','asc')->get();
         $ceding = CedingBroker::orderby('id','asc')->where('type',4)->get();
