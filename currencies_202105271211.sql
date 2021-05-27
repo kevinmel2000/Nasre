3b572c34ef3c17f3c2630aae8b965a8fb7bae5db
ahmad farhan h <ahmadfarhanhafidzudin555@gmail.com>
2021-05-27 12:12:42 +0700

    update sql

---
+++ currencies_202105271211.sql
@@ -0,0 +1,40 @@
+INSERT INTO pradev_mdp_core.currencies (symbol_name,is_base_currency,created_at,updated_at,code,country) VALUES
+	 ('United States of America Dollar','no','2021-01-07 15:57:48.0','2021-03-09 13:25:10.0','USD',231),
+	 ('Indonesia Rupiah','no','2021-01-15 09:09:15.0','2021-01-15 09:09:15.0','IDR',102),
+	 ('Singapore Dollar','no','2021-01-26 07:55:10.0','2021-03-09 13:24:48.0','SGD',196),
+	 ('Australia Dollar','no','2021-03-09 13:25:25.0','2021-03-09 13:25:25.0','AUD',13),
+	 ('Saudi Arabia Riyal','no','2021-03-09 13:25:44.0','2021-03-09 13:41:49.0','SAR',191),
+	 ('Afghanistan Afgani','no','2021-03-09 13:27:11.0','2021-03-09 13:27:11.0','AFA',1),
+	 ('Brunei Dollar','no','2021-03-09 13:27:59.0','2021-03-09 13:27:59.0','BND',32),
+	 ('Dutch Guilder','no','2021-03-09 13:32:22.0','2021-03-09 13:32:22.0','NLG',247),
+	 ('Canadian Dollar','no','2021-03-09 13:32:49.0','2021-03-09 13:32:49.0','CAD',38),
+	 ('Danish Krone','no','2021-03-09 13:35:53.0','2021-03-09 13:35:53.0','DKK',58);
+INSERT INTO pradev_mdp_core.currencies (symbol_name,is_base_currency,created_at,updated_at,code,country) VALUES
+	 ('Hongkong Dollar','no','2021-03-09 13:36:16.0','2021-03-09 13:36:16.0','HKD',98),
+	 ('India Rupee','no','2021-03-09 13:37:05.0','2021-03-09 13:37:05.0','INR',101),
+	 ('United Kingdom Poundsterling','no','2021-03-09 13:39:54.0','2021-03-09 13:39:54.0','GBP',230),
+	 ('Italian Lira','no','2021-03-09 13:40:15.0','2021-03-09 13:40:15.0','ITL',107),
+	 ('Iranian Rial','no','2021-03-09 13:40:46.0','2021-03-09 13:40:46.0','IRR',103),
+	 ('Iraqi Dinar','no','2021-03-09 13:41:39.0','2021-03-09 13:41:39.0','IQD',104),
+	 ('Deutsche Mark','no','2021-03-09 13:42:32.0','2021-03-09 13:42:32.0','DEM',82),
+	 ('Japan Yen','no','2021-03-09 13:42:53.0','2021-03-09 13:42:53.0','JPY',109),
+	 ('Kuwaiti Dinar','no','2021-03-09 13:43:23.0','2021-03-09 13:43:23.0','KWD',117),
+	 ('South Korean Won','no','2021-03-09 13:44:03.0','2021-03-09 13:44:03.0','KRW',116);
+INSERT INTO pradev_mdp_core.currencies (symbol_name,is_base_currency,created_at,updated_at,code,country) VALUES
+	 ('Malaysian Ringgit','no','2021-03-09 13:44:34.0','2021-03-09 13:44:34.0','MYR',132),
+	 ('New Zealand Dollar','no','2021-03-09 13:44:54.0','2021-03-09 13:44:54.0','NZD',157),
+	 ('French Franc','no','2021-03-09 13:45:27.0','2021-03-09 13:45:27.0','FRF',75),
+	 ('Philippine Peso','no','2021-03-09 13:45:53.0','2021-03-09 13:45:53.0','PHP',173),
+	 ('China Yuan','no','2021-03-09 13:46:15.0','2021-03-09 13:46:23.0','CNY',44),
+	 ('Swiss Franc','no','2021-03-09 13:46:50.0','2021-03-09 13:46:50.0','CHF',212),
+	 ('Spanish Peseta','no','2021-03-09 13:47:14.0','2021-03-09 13:47:14.0','ESP',205),
+	 ('Swedish Krona','no','2021-03-09 13:47:37.0','2021-03-09 13:47:37.0','SEK',211),
+	 ('Thailand Baht','no','2021-03-09 13:47:55.0','2021-03-09 13:47:55.0','THB',217),
+	 ('United Arab Emirates Dirham','no','2021-03-09 13:48:15.0','2021-03-09 13:48:15.0','AED',229);
+INSERT INTO pradev_mdp_core.currencies (symbol_name,is_base_currency,created_at,updated_at,code,country) VALUES
+	 ('Euro','no','2021-03-09 13:49:46.0','2021-03-09 13:49:46.0','EUR',251),
+	 ('Myanmar Kyat','no','2021-03-09 13:50:11.0','2021-03-09 13:50:11.0','MMK',150),
+	 ('Vietnamese Dong','no','2021-03-09 13:50:28.0','2021-03-09 13:50:28.0','VND',238),
+	 ('Papua New Guinea Kina','no','2021-03-09 13:50:47.0','2021-03-09 13:50:47.0','PGK',170),
+	 ('Nepalese Rupee','no','2021-03-09 13:51:05.0','2021-03-09 13:51:05.0','NPR',153),
+	 ('Bangladeshi Taka','no','2021-03-09 13:51:33.0','2021-03-09 13:51:33.0','BDT',18);
